d14937204047b1d1664af55337403351969b630a
Husnain Ali <ali@ili.digital>
2024-09-30 11:37:58 +0500

    testing

---
+++ PrecommitHooks/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  PrecommitHooks
+//
+//  Created by Husnain Ali - ILI on 30/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+        .onAppear {
+            add()
+        }
+    }
+
+    func add() {
+        let array: NSArray = NSArray(array: [5, 6, 7])
+        let numbers = array as! [Int]
+        print(numbers.reduce(0, +))
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrecommitHooks/PrecommitHooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrecommitHooksApp.swift
+//  PrecommitHooks
+//
+//  Created by Husnain Ali - ILI on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct PrecommitHooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
